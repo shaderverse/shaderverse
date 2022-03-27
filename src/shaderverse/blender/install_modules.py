8d6c4b2776bfd3092a4f4658bb42457a85066412
Michael Gold <goldmichael@gmail.com>
2022-03-27 17:31:13 -0400

    chore: uvicorn standard

--- src/shaderverse/blender/install_modules.py
+++ src/shaderverse/blender/install_modules.py
@@ -6,12 +6,13 @@ import sys
 
 def install_modules():
     python_path = sys.executable
-    required = {'uvicorn', 'fastapi', 'pydantic', 'pyngrok'}
+    required = {'uvicorn[standard]', 'fastapi', 'pydantic', 'pyngrok'}
 
     # subprocess.run([python_path, "-m", "ensurepip"], capture_output=True)
     # subprocess.run([python_path, "-m", "pip", "install", "--upgrade", "pip",])
     
     result = subprocess.run([python_path, "-m", "pip", "install", *required], capture_output=True)
+    print(result)
 
     
 if __name__ == "__main__":
