99b7d2206775e81bd92e84e7ca0cbf881a90cb3a
Michael Gold <goldmichael@gmail.com>
2022-03-27 15:18:22 -0400

    feat: generate preview url

--- src/shaderverse/blender/server.py
+++ src/shaderverse/blender/server.py
@@ -34,12 +34,12 @@ from shaderverse.blender.tunnel import Tunnel
 tunnel: Tunnel
 
 def init_fastapi():
-    # global fastapi
-    # fastapi = FastApiProcess()
-    # fastapi.start()
     print(bpy.app.version)
     controller.start(blender_binary_path=bpy.app.binary_path,blend_file=bpy.data.filepath )
     tunnel = Tunnel()
+    preview_url = f"https://shaderverse.com/preview/{tunnel.subdomain}"
+    # TODO write preview url to scene
+    # TODO open preview url
 
 def kill_fastapi():
     # global fastapi
