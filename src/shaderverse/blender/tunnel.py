ff58f75bbf422a31b95b7a33ebcfe6d6c876e98e
Michael Gold <goldmichael@gmail.com>
2022-03-27 15:06:29 -0400

    refactor: class documentation

--- src/shaderverse/blender/tunnel.py
+++ src/shaderverse/blender/tunnel.py
@@ -1,23 +1,25 @@
 from pyngrok import ngrok
 
 http_tunnel = None
-
-
 class Tunnel():
+    """ Provides ngrok tunnelling for Blender"""
     alive: bool
 
     def __init__(self):
+        """ Start the tunnel on port 8118 """
         self.process = ngrok.connect(8118, "http")
         self.subdomain = self.get_subdomain(self.process.public_url)
         self.alive = True
         print(self.subdomain)
 
     def get_subdomain(self, uri: str)-> str:
+        """ Get subdomain from public ngrok url """
         subdomain = uri.replace("http://", "")
         subdomain = subdomain.split(".")[0]
         return subdomain
     
     def kill(self):
+        """ Kill all ngrok processes """
         ngrok.kill()
         self.alive = False
 
