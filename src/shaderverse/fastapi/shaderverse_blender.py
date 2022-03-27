5d46fdc07529adfd8b092e3d5cae18b31476a1c1
Michael Gold <goldmichael@gmail.com>
2022-03-26 22:27:20 -0400

    refactor: remove dead code

--- src/shaderverse/fastapi/shaderverse_blender.py
+++ src/shaderverse/fastapi/shaderverse_blender.py
@@ -23,52 +23,16 @@ async def generate():
     print(metadata)
     return metadata
 
-
-# @app.post("/read_homefile")
-#     # ...
-
-# @app.post("/create_cube")
-#     # ...
-
-# @app.post("/render")
-#     # ...
-
-
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Python script to bootstrap Uvicorn')
-
-    # parser.add_argument('--app', 
-    #                 help='the app')
-
-    # parser.add_argument('--module', 
-    #                     help='the module',
-    #                     default="app")
-
-    # parser.add_argument('--host', 
-    #                     help='the ip addreess', 
-    #                     default="0.0.0.0")
 
     parser.add_argument('--port', 
                         help='the port', 
                         default=8119)
 
-    # parser.add_argument('--reload', 
-    #                     help='whether we should reload',
-    #                     action='store_false')
-
-    # parser.add_argument('--debug', 
-    #                     help='run in debug mode?',
-    #                     action='store_false')
-
-    # parser.add_argument('--workers', 
-    #                     help='number of workers',
-    #                     default=3)
-
     args, unknown = parser.parse_known_args()
 
     port = args.port
-
-    # uvicorn.run(app="shaderverse_blender:app", app_dir=SCRIPT_PATH, host="0.0.0.0", port=args.port)
 
     uvicorn.run(app="shaderverse_blender:app", app_dir=SCRIPT_PATH, host="0.0.0.0", port=args.port)
