eda5a43e8ce7fd104214f5b9b0ee7b27f8d2a523
Michael Gold <goldmichael@gmail.com>
2022-03-27 13:19:59 -0400

    build: version bump

--- src/shaderverse/__init__.py
+++ src/shaderverse/__init__.py
@@ -12,7 +12,7 @@ bl_info = {
   "name": "Shaderverse",
   "description": "Create parametricly driven NFTs using Geometry Nodes",
   "author": "Michael Gold",
-  "version": (1, 0, 11, 9),
+  "version": (1, 0, 11, 10),
   "blender": (3, 1, 0),
   "location": "Object > Modifier",
   "warning": "",
