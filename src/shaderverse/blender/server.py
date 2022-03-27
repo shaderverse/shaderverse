3795df76b59c8c64d0d93270e2a9cece77ada692
Michael Gold <goldmichael@gmail.com>
2022-03-27 15:18:54 -0400

    refactor: remove dead code

--- src/shaderverse/blender/server.py
+++ src/shaderverse/blender/server.py
@@ -5,32 +5,6 @@ import bpy
 from shaderverse.fastapi import controller
 from shaderverse.blender.tunnel import Tunnel
 
-
-# class FastApiProcess():
-
-#     data = None
-#     process = None
-#     error = None
-#     output = None
-
-#     def __init__(self):
-#         addon_file = os.path.realpath(__file__)
-#         addon_dir = os.path.dirname(addon_file)
-#         self.script_path = os.path.join(addon_dir, "fastapi/app.py")
-
-
-#     def start(self):
-#         """ Start the process """
-#         self.process = subprocess.Popen([sys.executable, self.script_path], stderr=self.error, stdout=self.output, shell=True)
-
-#         self.running = True
-
-
-#     def stop(self):
-#         """ Stop the process """
-#         self.process.kill()
-#         self.running = False
-
 tunnel: Tunnel
 
 def init_fastapi():
@@ -42,9 +16,6 @@ def init_fastapi():
     # TODO open preview url
 
 def kill_fastapi():
-    # global fastapi
-    # fastapi = FastApiProcess()
-    # fastapi.start()
     controller.proxy.process.kill()
     tunnel.kill()
 
