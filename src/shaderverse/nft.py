997cd456c3dcb785876c720ea24f44cd81a6cfa9
Michael Gold <goldmichael@gmail.com>
2022-04-08 15:54:09 -0400

    refactor: scaffold nft class

---
+++ src/shaderverse/nft.py
@@ -0,0 +1,35 @@
+import bpy
+
+class NFT():
+    metadata = {}
+    directory = ""
+    id: int
+
+    def __init__(self):
+        pass
+
+    def generate_metadata(self):
+        pass
+
+    def load_metadata(self, filename: str):
+        pass
+
+    def export_metadata(self):
+        pass
+
+    def render_preview(self):
+        pass
+
+    def render_glb(self):
+        pass
+
+    def render_jpeg(self):
+        pass
+
+
+
+
+
+
+
+
