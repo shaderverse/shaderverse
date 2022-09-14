15ad5679386a919faf60bd96523c145d96771858
Michael Gold <goldmichael@gmail.com>
2022-09-14 11:41:44 -0400

    feat: update api model

--- src/shaderverse/api/model.py
+++ src/shaderverse/api/model.py
@@ -1,6 +1,6 @@
 
 from pydantic import UUID4, BaseModel
-
+from enum import Enum, IntEnum
 class BlenderData(BaseModel):
     blend_file: str = None
     blender_binary_path: str = None
@@ -10,3 +10,18 @@ class Session(BaseModel):
     id: UUID4
     port: int 
     blend_file: str = None
+
+class SessionStatus(str, Enum):
+    ready = 'ready'
+    running = 'running'
+    finished = 'finished'
+
+class Action(str, Enum):
+    render_glb = 'render_glb'
+
+
+class SessionData(BaseModel):
+    status: SessionStatus = "ready"
+    total_count: int = 0
+    current_count: int = 0
+    current_id: int = 0
