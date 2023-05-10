06a84d75edfbcc455d6a628f3645808a446b88ed
Michael Gold <goldmichael@gmail.com>
2023-05-10 10:20:00 -0400

    feat: reload scripts after modules install

--- src/shaderverse/blender/install_modules.py
+++ src/shaderverse/blender/install_modules.py
@@ -2,7 +2,21 @@ import re
 import subprocess
 import sys
 import platform
+import bpy
 from  ..background.process import Process
+
+process = Process()
+
+def handle_module_install_process():
+    """Check the module install process every second and reload the scrips if completed"""
+    print(f"Result: {process.result}")
+    print(f"Status: {process.status}")
+    if process.status == "completed":
+        print("Modules installed")
+        # bpy.context.scene.shaderverse.is_modules_installed = True
+        bpy.ops.script.reload()
+        return None
+    return 1.0
 
 def install_modules():
     python_path = sys.executable
@@ -20,7 +34,8 @@ def install_modules():
     #     case "Darwin":
     #         required.add('ray[serve] @ https://s3-us-west-2.amazonaws.com/ray-wheels/latest/ray-2.0.0.dev0-cp310-cp310-macosx_10_15_universal2.whl')
     cmd = [python_path, "-m", "pip", "install", *required]
-    process = Process(cmd)
+    process.cmd = cmd
+    bpy.app.timers.register(handle_module_install_process)
     process.execute()
     print("Installing modules...")
     print(f"Result: {process.result}")
