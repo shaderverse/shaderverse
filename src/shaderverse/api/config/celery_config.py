e7b931d0b8eaa801ae254cf526a8c3fbbe1c2170
Michael Gold <goldmichael@gmail.com>
2023-05-21 08:49:44 -0400

    feat: use custom temdir path for celerydb

--- src/shaderverse/api/config/celery_config.py
+++ src/shaderverse/api/config/celery_config.py
@@ -1,7 +1,8 @@
 import os
 from functools import lru_cache
 from kombu import Queue
-
+from pathlib import Path
+from tempfile import gettempdir
 
 def route_task(name, args, kwargs, options, task=None, **kw):
     print(f"Routing task: {name}")
@@ -14,13 +15,17 @@ def route_task(name, args, kwargs, options, task=None, **kw):
 class BaseConfig:
     # CELERY_broker_url: str = os.environ.get("CELERY_broker_url", "amqp://guest:guest@localhost:5672//")
     # result_backend: str = os.environ.get("result_backend", "rpc://")
-
-    broker_url: str = os.environ.get("CELERY_broker_url", "sqla+sqlite:///celerydb.sqlite")
+    tempdir = Path(gettempdir())
+    db_path = tempdir.joinpath("celerydb.sqlite")
+    db_url = f"sqla+sqlite:///{str(db_path)}"
+   
+    broker_url: str = os.environ.get("CELERY_broker_url", db_url)
 
     # result_backend: str = os.environ.get("db+sqlite:///results.sqlite")
     CELERY_BROKER_BACKEND = "db+sqlite:///celery.sqlite"
     cache_backend = "db+sqlite:///celery.sqlite"
     result_backend = "db+sqlite:///celery.sqlite"
+    worker_send_task_events = True
 
     CELERY_TASK_QUEUES: list = (
         # default queue
